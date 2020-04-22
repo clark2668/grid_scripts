566cb7c89694eb756429d8883e3e5801e383f729
clark2668 <baclark@msu.edu>
2020-04-21 21:57:36 -0500

    add basic version of submit scripts to do stuff with pyrex; they won't work yet, but I've gotta get something ready

---
+++ pyrex_sims/job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+energy=$1
+index=$2
+working_dir=$PWD
+
+source /mnt/research/IceCube/baclark/IceCube/radio_sims/tools/env.sh
+
+cd /mnt/research/IceCube/baclark/IceCube/radio_sims/scripts
+
+python run_sim.py -e $energy -i $index -n 1000 -o $working_dir
+
+cd $working_dir
+chmod -R 777 *.log
+chmod -R 777 *.hdf5
+
+mv $working_dir/*.log  /mnt/research/IceCube/baclark/IceCube/radio_sims/ouptut/E${energy}/
+mv $working_dir/*.hdf5 /mnt/research/IceCube/baclark/IceCube/radio_sims/ouptut/E${energy}/
+
