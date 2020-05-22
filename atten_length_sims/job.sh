dfc2e73e42b236cdfee4043c93aa48fc636a0f01
clark2668 <baclark@msu.edu>
2020-05-22 13:15:15 -0500

    update some things

--- atten_length_sims/job.sh
+++ atten_length_sims/job.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 version=$1
 energy=$2
 index=$3
 working_dir=$PWD
 
+echo "Version: " $version
+echo "Energy: " $energy
+echo "Index: " $index
+
+printf "User is: "; whoami
+printf "Host is: "; hostname
+
 source /mnt/research/IceCube/baclark/ara/atten_length_comparison/env.sh
+#. /mnt/research/IceCube/baclark/ara/atten_length_comparison/env.sh
 
 export setup_file='/mnt/research/IceCube/baclark/ara/atten_length_comparison/sims/setups/setup_E'$energy'.txt'
 
 cd /mnt/research/IceCube/baclark/ara/atten_length_comparison/sims/$version
+
+echo $PWD
 
 ./AraSim $setup_file $index $working_dir
 
