1c37c29c665d05e886a901bd0fb67d0142f5a9c8
clark2668 <baclark@msu.edu>
2020-06-02 15:40:41 -0500

    make a gen2 radiu comparison folder

---
+++ gen2radio_comparison/job.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+energy=$1
+index=$2
+working_dir=$PWD
+
+echo "Version: " $version
+echo "Energy: " $energy
+echo "Index: " $index
+
+printf "User is: "; whoami
+printf "Host is: "; hostname
+
+source /etc/profile.d/modules.sh
+source /mnt/research/IceCube/baclark/IceCube/work_with_jorge/env.sh
+
+export setup_file='/mnt/research/IceCube/baclark/IceCube/work_with_jorge/comparison_studies/SETUP/sim_comparison/setupRNO_comparison_E'$energy'.txt'
+
+cd /mnt/research/IceCube/baclark/IceCube/work_with_jorge/comparison_studies
+
+index=$((index+2000))
+
+./AraSim $setup_file $index $working_dir
+
+cd $working_dir
+chmod -R 777 *.root
+chmod -R 777 *
+
+cp $working_dir/*.root /mnt/research/IceCube/baclark/IceCube/work_with_jorge/output/E${energy}/${sigma}
+rm *.root
